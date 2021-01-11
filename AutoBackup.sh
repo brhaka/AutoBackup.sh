cbba01f9246a41276d7fedfef7733bb00c4f5009
Eduardo Julião Martinez <martinez@brhaka.com>
2021-01-11 13:12:25 +0000

    AutoBackup now zips the backup

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -7,12 +7,11 @@ then
 else
 	if [ -d "/Volumes/$BACKUP_DRIVE/" ]
 	then	
-		export BACKUP_DATETIME_FOLDER_NAME="$(date +'%Y-%m-%d__%H-%M-%S')"
-		mkdir /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME
-		cd /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME/
-		rsync -a -v --exclude='/Library' ~/ /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME
-		printf "Backup successfully done.\nFiles were copied to $BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME."
-		unset BACKUP_DATETIME_FOLDER_NAME
+		export BACKUP_DATETIME_FILE_NAME="$(date +'%Y-%m-%d__%H-%M-%S')"
+		cd /Volumes/$BACKUP_DRIVE/
+		zip -r /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FILE_NAME.zip ~/ -x '*Library*'
+		printf "Backup successfully done.\nFiles were zipped to $BACKUP_DRIVE/$BACKUP_DATETIME_FILE_NAME.zip"
+		unset BACKUP_DATETIME_FILE_NAME
 	else
 		printf "Drive specified in variable \"BACKUP_DRIVE\" does not exists."
 	fi
