14b1674314abb3dc33c4351161491742b10f1fe3
Brhaka <martinez@brhaka.com>
2021-01-11 12:34:23 +0000

    Fixed \n being printed instead of new line

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -3,7 +3,7 @@
 
 if [ -z ${BACKUP_DRIVE} ]
 then
-	echo "Variable \"BACKUP_DRIVE\" does not exists."
+	printf "Variable \"BACKUP_DRIVE\" does not exists."
 else
 	if [ -d "/Volumes/$BACKUP_DRIVE/" ]
 	then	
@@ -11,9 +11,9 @@ else
 		mkdir /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME
 		cd /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME/
 		rsync -a -v --exclude='/Library' ~/ /Volumes/$BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME
-		echo "Backup successfully done.\nFiles were copied to $BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME."
+		printf "Backup successfully done.\nFiles were copied to $BACKUP_DRIVE/$BACKUP_DATETIME_FOLDER_NAME."
 		unset BACKUP_DATETIME_FOLDER_NAME
 	else
-		echo "Drive specified in \"\$BACKUP_DRIVE\" does not exists."
+		printf "Drive specified in \"\$BACKUP_DRIVE\" does not exists."
 	fi
 fi
